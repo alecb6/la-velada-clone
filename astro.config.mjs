87c0e78d46d1bdcfed0fd02fb2a4487fca1c916d
Alejandro Cruz <alejandro.cruz@grupoexact.com>
2025-04-04 09:57:17 +0200

    Update Astro configuration and add tailwind-animations dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   experimental: {
     svg: {
-      mode: "sprite"
-    }
-  }
+      mode: "sprite",
+    },
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
+  site: "https://la-velada-clone.vercel.app/",
 });
