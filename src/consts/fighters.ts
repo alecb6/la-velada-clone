1ed14bf9967081fc425c4dfbe1fddb9f71bf78ed
Alejandro Cruz <alejandro.cruz@grupoexact.com>
2025-04-04 09:57:58 +0200

    Add fighter and combat pages, enhance layout with Hero component, and improve sponsors section

---
+++ src/consts/fighters.ts
@@ -0,0 +1,58 @@
+export const FIGHTERS = [
+  {
+    id: "peereira",
+    name: "Pereira",
+  },
+  {
+    id: "perxitaa",
+    name: "Perxitaa",
+  },
+  {
+    id: "abby",
+    name: "Abby",
+  },
+  {
+    id: "roro",
+    name: "Roro",
+  },
+  {
+    id: "gaspi",
+    name: "Gaspi",
+  },
+  {
+    id: "rivaldios",
+    name: "Rivaldios",
+  },
+  {
+    id: "andoni",
+    name: "Andoni",
+  },
+  {
+    id: "viruzz",
+    name: "Viruzz",
+  },
+  {
+    id: "alana",
+    name: "Alana",
+  },
+  {
+    id: "grefg",
+    name: "TheGrefg",
+  },
+  {
+    id: "westcol",
+    name: "WestCol",
+  },
+  {
+    id: "arigeli",
+    name: "AriGeli",
+  },
+  {
+    id: "tomas",
+    name: "Tomás",
+  },
+  {
+    id: "carlos",
+    name: "Carlos",
+  },
+];
